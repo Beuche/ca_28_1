a2fae2cddae665f2a7cc9ac246ed6699773a220e
Beuche <76558020+Beuche@users.noreply.github.com>
2020-12-24 15:49:53 +0100

    Aktualisiere main.ts, main.py

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 licht=0
 lampe=0
 while True:
-    _4digit = grove.create_display(DigitalPin.C16, DigitalPin.C17) 
-    _my4_digit.show(0)
+    licht=input.light_level()
+    if licht<100:
+        lampe=255-licht
+        basic.set_led_color(__internal.__colorNumberPicker(basic.rgbw(lampe, lampe, lampe, 0)))
+    else:
+        basic.set_led_color(__internal.__colorNumberPicker(0x000000))
+    anzeige = grove.create_display(DigitalPin.C16, DigitalPin.C17) 
+    anzeige.set(7)
+    anzeige.show(input.light_level())
+    
--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
-let _4digit: grove.TM1637;
+let anzeige: grove.TM1637;
 let licht = 0
 let lampe = 0
 while (true) {
-    _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
+    licht = input.lightLevel()
+    if (licht < 100) {
+        lampe = 255 - licht
+        basic.setLedColor(__internal.__colorNumberPicker(basic.rgbw(lampe, lampe, lampe, 0)))
+    } else {
+        basic.setLedColor(__internal.__colorNumberPicker(0x000000))
+    }
+    
+    anzeige = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
+    anzeige.set(7)
+    anzeige.show(input.lightLevel())
 }
