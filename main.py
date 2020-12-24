c9a4bcc83034d8e5dd91f3829d272292fd8f48ac
Beuche <76558020+Beuche@users.noreply.github.com>
2020-12-24 15:39:50 +0100

    Aktualisiere pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 licht=0
 lampe=0
 while True:
-    licht=input.light_level()
-    if licht>50:
-        basic.set_led_color(__internal.__colorNumberPicker(0x000000))
-    else:
-        basic.set_led_color(__internal.__colorNumberPicker(basic.rgbw(licht, licht, licht, 0)))
+    _4digit = grove.create_display(DigitalPin.C16, DigitalPin.C17) 
+    _my4_digit.show(0)
--- main.ts
+++ main.ts
@@ -1,11 +1,6 @@
+let _4digit: grove.TM1637;
 let licht = 0
 let lampe = 0
 while (true) {
-    licht = input.lightLevel()
-    if (licht > 50) {
-        basic.setLedColor(__internal.__colorNumberPicker(0x000000))
-    } else {
-        basic.setLedColor(__internal.__colorNumberPicker(basic.rgbw(licht, licht, 255, 0)))
-    }
-    
+    _4digit = grove.createDisplay(DigitalPin.C16, DigitalPin.C17)
 }
